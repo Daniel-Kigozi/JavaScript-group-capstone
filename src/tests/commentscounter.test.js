647d92e8da6e303d5e35ed103e49a60d889aea4a
Daniel-Kigozi <kdanalex69@gmail.com>
2023-01-12 19:04:06 +0300

    add comment counter tests

---
+++ src/tests/commentscounter.test.js
@@ -0,0 +1,15 @@
+import commentCounter from '../modules/commentscounter.js';
+
+describe('commentCounter function returns number of comments in an array', () => {
+  it('should return the number of comments in an array', () => {
+    expect(commentCounter()).toBe(3);
+  });
+
+  it('should return 0 if the input is not an array', () => {
+    expect(commentCounter(undefined)).toEqual(0);
+  });
+
+  it('should return 0 if the array is empty', () => {
+    expect(commentCounter([])).toEqual(0);
+  });
+});
