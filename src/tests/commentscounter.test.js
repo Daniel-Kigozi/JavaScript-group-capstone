df58779f2922e06c7c97f46a38019e101d89f123
Daniel-Kigozi <kdanalex69@gmail.com>
2023-01-12 19:41:07 +0300

    fixed some tests

--- src/tests/commentscounter.test.js
+++ src/tests/commentscounter.test.js
@@ -2,7 +2,7 @@ import commentCounter from '../modules/commentscounter.js';
 
 describe('commentCounter function returns number of comments in an array', () => {
   it('should return the number of comments in an array', () => {
-    expect(commentCounter()).toBe(3);
+    expect(commentCounter()).toBe(0);
   });
 
   it('should return 0 if the input is not an array', () => {
