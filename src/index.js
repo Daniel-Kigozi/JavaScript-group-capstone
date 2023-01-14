c4043dcecdde583741f58e1df4ea01e1d4903160
M.Nisar Ali <101400298+MNisarAli@users.noreply.github.com>
2023-01-14 11:53:02 +0500

    Fix Tv shows counter UI display issue

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 import './style.css';
 import createShows from './modules/create-shows.js';
 import getShows from './modules/base-api.js';
+import { itemsCounter } from './modules/items-counter.js';
 
+const tvShows = document.querySelector('.shows')
 const allShowsContainer = document.querySelector('.shows-list');
 const main = async () => {
   const shows = await getShows();
   const numberOfShows = shows.slice(10, 33);
   createShows(numberOfShows, allShowsContainer);
+  tvShows.innerHTML = `TV Shows (${itemsCounter(numberOfShows)})`;
 };
 
 window.addEventListener('load', main);
